718052849137456750dfe1c6bb868a207eff2bf3
Pranav Yeole <yeolepranav13@gmail.com>
2026-04-07 22:38:28 +0530

    Update isAuth.js

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,25 +1,3 @@
-// import jwt from "jsonwebtoken";
-
-// const isAuth = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.token;
-//     if (!token) {
-//       return res.status(401).json({ message: "token not found" });
-//     }
-//     const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
-//     if (!decodeToken) {
-//       return res.status(401).json({ message: "token not verify" });
-//     }
-//     req.userId = decodeToken.userId;
-//     next();
-//   } catch (error) {
-//     return res.status(500).json({ message: "isAuth error" });
-//   }
-// };
-
-// export default isAuth;
-
-
 import jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
